1feeddebc8949410890223445a1c14763aab324d
sankalp123321 <sankalp13392>
2021-11-30 11:30:19 -0700

    chore: flash programming working

--- Bootloader_PES/source/bootloader/bootloader.c
+++ Bootloader_PES/source/bootloader/bootloader.c
@@ -1,4 +1,5 @@
 #include "bootloader.h"
+#include "../flash/flash.h"
 #include <stdio.h>
 
 static bootstates_t g_state = eCHECKFORINPUT;
@@ -25,20 +26,29 @@ void Bootloader_StateMachine()
     switch (g_state)
     {
     case eERASE:
+    	g_enableTimer = 0;
         // erase the flash
+    	Flash_write(0x7800, 30);
+//    	for(int i = 0; i < 10000000; i++){for(int j = 0; j < 100; j++){}}
+//    	Flash_erase(0x7800);
+    	int *a = (int*)(0x00007800);
+    	printf("%08X\r\n", *a);
+//    	for(int i = 0; i < 10000000; i++){for(int j = 0; j < 100; j++){}}
+    	g_state = 0xFF;
         break;
     case ePROGRAMFLASH:
+    	g_enableTimer = 0;
         // wait for the .s19
         break;
     case eBOOTAPPL:
-        printf("Entering boot applicaton mode\r\n");
+        printf("Entering boot application mode\r\n");
         g_enableTimer = 0;
         g_state = 0xFF;
         break;
     case eCHECKFORINPUT:
         printf("Entering check for input mode\r\n");
         g_enableTimer = 1;
-        // check for 20 seconds for uart command, if the user has not pressed anything
+        // check for 20 seconds for UART command, if the user has not pressed anything
         // then boot the application
         g_bootApplTimer = 20;
         g_state = 0xFF;
--- Bootloader_PES/source/flash/flash.c
+++ Bootloader_PES/source/flash/flash.c
@@ -5,6 +5,7 @@
  *      Author: Sankalp
  */
 #include "flash.h"
+#include "../drivers/fsl_common.h"
 #include "MKL25Z4.h"
 
 int Flash_erase(int sector)
@@ -12,21 +13,18 @@ int Flash_erase(int sector)
     uint8_t byte_16 = (sector << 8) >> 24;
     uint8_t byte_8 = (sector << 16) >> 24;
     uint8_t byte_0 = (sector << 24) >> 24;
-    while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
 
-    if((FTFA->FSTAT & FTFA_FSTAT_FPVIOL_MASK) ||
-    (FTFA->FSTAT & FTFA_FSTAT_ACCERR_MASK))
-    {
-        FTFA->FSTAT = 0x30;
-    }
+
+    FTFA->FSTAT = FTFA_FSTAT_FPVIOL_MASK | FTFA_FSTAT_ACCERR_MASK | FTFA_FSTAT_RDCOLERR_MASK;
+
+    while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
 
     FTFA->FCCOB0 = 9;
     FTFA->FCCOB1 = byte_16;
     FTFA->FCCOB2 = byte_8;
     FTFA->FCCOB3 = byte_0;
 
-    FTFA->FSTAT |= FTFA_FSTAT_CCIF_MASK;
-    FTFA->FSTAT = 0x80;
+    FTFA->FSTAT = FTFA_FSTAT_CCIF_MASK;
 
     while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
     return 1;
@@ -34,7 +32,7 @@ int Flash_erase(int sector)
 
 int Flash_read(int sector, int* value)
 {
-
+	return 0;
 }
 
 int Flash_write(int sector, int value)
@@ -47,13 +45,8 @@ int Flash_write(int sector, int value)
     uint8_t value_byte_16 = (value << 8) >> 24;
     uint8_t value_byte_8 = (value << 16) >> 24;
     uint8_t value_byte_0 = (value << 24) >> 24;
+    int a = DisableGlobalIRQ();
     while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
-
-    if((FTFA->FSTAT & FTFA_FSTAT_FPVIOL_MASK) ||
-    (FTFA->FSTAT & FTFA_FSTAT_ACCERR_MASK))
-    {
-        FTFA->FSTAT = 0x30;
-    }
 
     FTFA->FCCOB0 = 6;
     FTFA->FCCOB1 = byte_16;
@@ -64,9 +57,10 @@ int Flash_write(int sector, int value)
     FTFA->FCCOB6 = value_byte_16;
     FTFA->FCCOB7 = value_byte_24;
 
-    FTFA->FSTAT |= FTFA_FSTAT_CCIF_MASK;
-    FTFA->FSTAT = 0x80;
+    FTFA->FSTAT = FTFA_FSTAT_FPVIOL_MASK | FTFA_FSTAT_ACCERR_MASK | FTFA_FSTAT_RDCOLERR_MASK;
 
-    while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
+	FTFA->FSTAT = FTFA_FSTAT_CCIF_MASK;
+	while(!(FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK)) {}
+	EnableGlobalIRQ(a);
     return 1;
 }
